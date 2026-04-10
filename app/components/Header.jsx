8eaff630d16cb88648a80fdb7f5cfef6a57be303
Moriam Akter Swarna <moriam15-12880@diu.edu.bd>
2026-04-10 12:21:19 +0600

    Code for video-2 included

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import logoImg from "../assets/logo.png";
+import { format } from "date-fns";
+const today = format(new Date(), "EEEE, MMMM dd, yyyy");
 
 export default function Header() {
   return (
@@ -13,6 +15,8 @@ export default function Header() {
         className="mx-auto w-auto max-w-87.5 h-auto"
       />
       <p className="mt-2">Journalism Without Fear or Favour</p>
+
+      <p className="text-sm text-muted-foreground mt-4">{today}</p>
     </div>
   );
 }
